a3e559ade5a9c8a92352b670f835ea93bd6edb6b
Mik <29287456+Mikulasz12@users.noreply.github.com>
2026-07-28 00:50:18 +0100

    fix: register public commands in configured guild

--- src/index.js
+++ src/index.js
@@ -73,14 +73,20 @@ registerGuidanceEvents(client, {
 });
 
 client.once(Events.ClientReady, async (readyClient) => {
+  const infoCommand = infoCommandData.toJSON();
+  const uptimeCommand = uptimeCommandData.toJSON();
   await readyClient.application.commands.set([
     scanCommandData.toJSON(),
-    infoCommandData.toJSON(),
-    uptimeCommandData.toJSON(),
+    infoCommand,
+    uptimeCommand,
   ]);
-  await readyClient.application.commands.set([cacheCommandData.toJSON()], config.allowedGuildId);
+  await readyClient.application.commands.set([
+    cacheCommandData.toJSON(),
+    infoCommand,
+    uptimeCommand,
+  ], config.allowedGuildId);
   console.log(
-    `Ready as ${readyClient.user.tag}; /scan, /info, and /uptime registered globally and /cache registered in guild ${config.allowedGuildId}.`,
+    `Ready as ${readyClient.user.tag}; /scan, /info, and /uptime registered globally; /cache, /info, and /uptime registered immediately in guild ${config.allowedGuildId}.`,
   );
 });
 
